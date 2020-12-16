6b7030fdb994b9c5977995c7b685ffeb417d86cd
Pascanu Stefan <virgil_stefan2000@yahoo.com>
2020-12-16 13:12:24 +0200

    CAPI-3 Created Shuttle Migration , updated MySQL database

---
+++ ProjectAstra.Web.CrewApi/ProjectAstra.Web.CrewApi.Presentation/Migrations/20201216110648_AddShuttleDbSet.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProjectAstra.Web.CrewApi.Presentation.Migrations
+{
+    public partial class AddShuttleDbSet : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Shuttles",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    MaxCrewCapacity = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Shuttles", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Shuttles_Name",
+                table: "Shuttles",
+                column: "Name");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Shuttles");
+        }
+    }
+}
