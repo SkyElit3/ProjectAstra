aebfab396c73105b059f9424ac6e75c50dc8dee5
Pascanu Stefan <virgil_stefan2000@yahoo.com>
2020-12-16 14:41:45 +0200

    CAPI-6 Created ShuttleRepo and IShuttleRepo

---
+++ ProjectAstra.Web.CrewApi/ProjectAstra.Web.CrewApi.Core/Interfaces/IShuttleRepo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ProjectAstra.Web.CrewApi.Core.Models;
+
+namespace ProjectAstra.Web.CrewApi.Core.Interfaces
+{
+    public interface IShuttleRepo
+    {
+        public Task<Shuttle> GetShuttle(Guid id);
+
+        public Task<List<Shuttle>> GetAllShuttles();
+
+        public Task<Shuttle> CreateShuttle(Shuttle inputShuttle);
+
+        public Task<Shuttle> DeleteShuttle(Guid id);
+
+        public Task<Shuttle> UpdateShuttle(Shuttle inputShuttle);
+    }
+}
---
+++ ProjectAstra.Web.CrewApi/ProjectAstra.Web.CrewApi.Infrastructure/Repositories/ShuttleRepo.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjectAstra.Web.CrewApi.Core.Interfaces;
+using ProjectAstra.Web.CrewApi.Core.Models;
+
+namespace ProjectAstra.Web.CrewApi.Infrastructure.Repositories
+{
+    public class ShuttleRepo : IShuttleRepo
+    {
+        private readonly DataContext.DataContext _dataContext;
+
+        public ShuttleRepo(DataContext.DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        public async Task<Shuttle> GetShuttle(Guid id)
+        {
+            return await _dataContext.Shuttles.FirstOrDefaultAsync(t => t.Id.Equals(id));
+        }
+
+        public async Task<List<Shuttle>> GetAllShuttles()
+        {
+            return await Task.Run(() => _dataContext.Shuttles.ToListAsync());
+        }
+
+        public async Task<Shuttle> CreateShuttle(Shuttle inputShuttle)
+        {
+            return await Task.Run(async () =>
+            {
+                await _dataContext.Shuttles.AddAsync(inputShuttle);
+                await _dataContext.SaveChangesAsync();
+                return inputShuttle;
+            });
+        }
+
+        public async Task<Shuttle> DeleteShuttle(Guid id)
+        {
+            var toDeleteShuttle = await _dataContext.Shuttles.FirstOrDefaultAsync(t => t.Id.Equals(id));
+            _dataContext.Shuttles.Remove(toDeleteShuttle);
+            await _dataContext.SaveChangesAsync();
+            return toDeleteShuttle;
+        }
+
+        public async Task<Shuttle> UpdateShuttle(Shuttle inputShuttle)
+        {
+            var updatedShuttle = await Task.Run(() =>
+            {
+                return _dataContext.Shuttles.Where(b => b.Id.Equals(inputShuttle.Id)).ToList().Select(s =>
+                {
+                    // TODO: add update by reflection
+                    s.Name = inputShuttle.Name;
+                    s.MaxCrewCapacity = inputShuttle.MaxCrewCapacity;
+                    s.TeamOfExplorers = inputShuttle.TeamOfExplorers;
+                    return s;
+                }).First();
+            });
+            await _dataContext.SaveChangesAsync();
+            return updatedShuttle;
+        }
+    }
+}
